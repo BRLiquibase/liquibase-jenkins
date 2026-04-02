3f1ce80daa3dc4a8c5ce292399ea18820f51cbcb
BRLiquibase <briley@liquibase.com>
2026-04-02 07:21:21 +0100

    rugby table

--- changelog/changeset.sql
+++ changelog/changeset.sql
@@ -1,6 +1,6 @@
 --liquibase formatted sql
 
---changeset benriley:1774379681428
+--changeset benriley:1CreateAussieRulesTable
 --description Insert SQL change objects here https://docs.liquibase.com/change-types/home.html
 CREATE TABLE AussieRules (
     id SERIAL PRIMARY KEY,
@@ -10,10 +10,21 @@ CREATE TABLE AussieRules (
 
 --rollback DROP TABLE AussieRules;
 
---changeset benriley:1774379681429
+--changeset benriley:2CreateCricketTable
 create table Cricket (
     id SERIAL PRIMARY KEY,
     rule_name VARCHAR(255) NOT NULL,
     description TEXT
 );
 --rollback DROP TABLE Cricket;
+
+
+--changeset benriley:3CreateRugbyTable
+CREATE table Rugby (
+    id SERIAL PRIMARY KEY,
+    rule_name VARCHAR(255) NOT NULL,
+    description TEXT
+);
+--rollback DROP TABLE Rugby;
+
+
