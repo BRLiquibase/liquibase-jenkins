f6e86f399c07fe7ffacaed4b6b17aa1f569477b2
BRLiquibase <briley@liquibase.com>
2026-07-02 18:41:17 +0100

    new code to deploy

--- changelog/changelog.demo.sql
+++ changelog/changelog.demo.sql
@@ -1,12 +1,16 @@
 --liquibase formatted sql
 
---changeset benriley:CreateDemoTable labels:demo context:dev,test
---comment Create demo table
-CREATE TABLE demo (
-  demo_id UUID NOT NULL PRIMARY KEY,
+--changeset benriley:CreateYetiTable labels:demo context:dev,test
+--comment Create PostgreSQL demo table yeti
+CREATE TABLE yeti (
+  yeti_id UUID NOT NULL PRIMARY KEY,
   name varchar(100) NOT NULL,
+  habitat varchar(100) NOT NULL,
+  height_cm integer,
+  weight_kg numeric(7,2),
+  last_sighted_at timestamp NOT NULL DEFAULT now(),
   description text,
   created_at timestamp NOT NULL DEFAULT now(),
   updated_at timestamp NOT NULL DEFAULT now()
 );
---rollback DROP TABLE demo;
+--rollback DROP TABLE yeti;
